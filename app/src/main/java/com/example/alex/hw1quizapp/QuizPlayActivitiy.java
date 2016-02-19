a9fb66af98126d52313007dae72c28cb208b0b7f
Alex Kocher <akocher@wisc.edu>
2016-02-19 15:21:24 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alex/hw1quizapp/QuizPlayActivitiy.java
@@ -0,0 +1,34 @@
+package com.example.alex.hw1quizapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class QuizPlayActivitiy extends AppCompatActivity implements QuizFragment1.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_play_activitiy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //this can be empty
+    }
+}
